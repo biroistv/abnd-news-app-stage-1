d15ed9b63ac0108ac6356603a962d3d21e91a921
Biró István <biroistv@outlook.hu>
2018-07-18 13:26:26 +0200

    Added news opening
    #Added
        NewsAdapter:
            - added click listener on each item, if clicked it will open the new in the browser
    
    #Removed
    
    #Changed
    
    #Misc

--- app/src/main/java/com/example/biro/abnd_news_app_s1/News/NewsAdapter.java
+++ app/src/main/java/com/example/biro/abnd_news_app_s1/News/NewsAdapter.java
@@ -1,6 +1,8 @@
 package com.example.biro.abnd_news_app_s1.News;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -30,11 +32,20 @@ public class NewsAdapter extends ArrayAdapter<News> {
         if (view == null)
             view = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
 
-        News news = getItem(position);
+        final News news = getItem(position);
 
         ((TextView)view.findViewById(R.id.list_item_title)).setText(news.getTitle());
         ((TextView)view.findViewById(R.id.list_item_section)).setText(news.getSectionName());
         ((TextView)view.findViewById(R.id.list_item_pubdate)).setText(news.getPublicationDate());
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(news.getUrl().toString()));
+                getContext().startActivity(intent);
+            }
+        });
 
         return view;
     }
